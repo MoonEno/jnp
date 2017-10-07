a7b8deb21970e7117a882202e9fe1657c9aae6ca
moon <mooneknow@naver.com>
2017-10-07 22:11:32 +0900

    컨틀롤러 패키지 생성

---
+++ Jnp/src/jnp/com/controller/PageController.java
@@ -0,0 +1,19 @@
+/**
+ * (주)오픈잇 | http://www.openit.co.kr
+ * Copyright (c)2006-2017, openit Inc.
+ * All rights reserved.
+ */
+package jnp.com.controller;
+
+
+/**
+ * 내용 입력
+ *
+ * @author <a href="mailto:support@openit.co.kr">(주)오픈잇 | openit Inc.</a>
+ * @version 
+ * @since 
+ * @created 2017. 10. 7.
+ */
+public class PageController {
+
+}
